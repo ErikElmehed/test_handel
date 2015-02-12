c9ec134bb03d00edb76a08a98737f98a88dee456
agneslangerth <agneslangerth@Agness-MacBook-Pro.local>
2015-02-12 10:41:25 +0100

    conflict resolved

--- header.php
+++ header.php
@@ -1,12 +1,9 @@
 <html>
 <head>
 	<title>E-handel</title>
-<<<<<<< HEAD
 	<link rel="stylesheet" type="text/css" href="css/style.css">
-=======
 	<meta charset="UTF-8">
 
->>>>>>> FETCH_HEAD
 </head>
 <body>
 
