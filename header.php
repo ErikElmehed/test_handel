592240d64bf1282bdb8db27b1b886e0fcf3c89e4
Cecilia Fredriksson <c.fredrikssons@gmail.com>
2015-02-12 10:52:31 +0100

    nu blire kaaaos

--- header.php
+++ header.php
@@ -1,12 +1,10 @@
 <html>
 <head>
 	<title>E-handel</title>
-<<<<<<< HEAD
+
 	<link rel="stylesheet" type="text/css" href="css/style.css">
-=======
 	<meta charset="UTF-8">
 
->>>>>>> FETCH_HEAD
 </head>
 <body>
 
--- index.php
+++ index.php
@@ -1,18 +1,26 @@
 
 <?php require 'header.php'; ?> 
+<div class="section-wrapper">
+	<div class="onerow">
+		<div class="col6">
+			<h1>Välkommen till världens bästa E-handel</h1>
+			<h2>Här kan du hitta allt du vill mellan himmel och jord</h2>
+		</div>
+		<div class="col6 last">
+			<p>kom å kööp!</p>
+		</div>
 
-<h1>Välkommen till världens bästa E-handel</h1>
-<div class="content">
-	<?php
-	$drinks = array(
-		array('Coke zero', '50cl', 15),
-		array('Coke zero', '33cl', 10),
-		array('Beer', '50cl', 25),
-		array('Beer', '33cl', 10)
-		);
-		?>
+
+		<?php
+		$drinks = array(
+			array('Coke zero', '50cl', 15),
+			array('Coke zero', '33cl', 10),
+			array('Beer', '50cl', 25),
+			array('Beer', '33cl', 10)
+			);
+			?>
 	</div>
-
+</div>
 	<?php require 'footer.php'; ?> 
 
 
