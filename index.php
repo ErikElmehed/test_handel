2cddc0a0cb47e9677b854a2af67f768e94cd14fe
Cecilia Fredriksson <c.fredrikssons@gmail.com>
2015-02-12 10:57:14 +0100

    some fixing

--- index.php
+++ index.php
@@ -9,35 +9,19 @@
 		<div class="col6 last">
 			<p>kom å kööp!</p>
 		</div>
-
-<<<<<<< HEAD
-
-		<?php
-		$drinks = array(
-			array('Coke zero', '50cl', 15),
-			array('Coke zero', '33cl', 10),
-			array('Beer', '50cl', 25),
-			array('Beer', '33cl', 10)
-			);
-			?>
-	</div>
-</div>
+		<div class="onerow">
+			<div class="col8">
+				<?php
+					$drinks = array(
+						array('Coke zero', '50cl', 15),
+						array('Coke zero', '33cl', 10),
+						array('Beer', '50cl', 25),
+						array('Beer', '33cl', 10)
+						);
+						?>
+			</div>
+		</div>
 	<?php require 'footer.php'; ?> 
-=======
-<div class="content">
-	<h1>Välkommen till världens bästa E-handel</h1>
-	<?php
-	$drinks = array(
-		array('Coke zero', '50cl', 15),
-		array('Coke zero', '33cl', 10),
-		array('Beer', '50cl', 25),
-		array('Beer', '33cl', 10)
-		);
-		?>
-</div>
-
-<?php require 'footer.php'; ?> 
->>>>>>> FETCH_HEAD
 
 
 
