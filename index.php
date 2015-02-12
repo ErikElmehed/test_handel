35266815e15983f64b4664a3fd9e73aa224c2e49
inaschmitz <ina.e.schmitz@gmail.com>
2015-02-12 11:02:20 +0100

    loop

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 
 <?php require 'header.php'; ?> 
+
 <div class="section-wrapper">
 	<div class="onerow">
 		<div class="col6">
@@ -11,21 +12,31 @@
 		</div>
 		<div class="onerow">
 			<div class="col8">
+				<ul>
 				<?php
 					$drinks = array(
-						array('Coke zero', '50cl', 15),
-						array('Coke zero', '33cl', 10),
-						array('Beer', '50cl', 25),
-						array('Beer', '33cl', 10)
+						array('Coke zero', 50, 15),
+						array('Coke zero', 33, 10),
+						array('Beer', 50, 25),
+						array('Beer', 33, 10)
 						);
-						?>
+				
+
+				for ($i=0; $i<count($drinks); $i++) { 
+					$name = $drinks[$i][0];
+					$size = $drinks[$i][1];
+					$price = $drinks[$i][2];
+
+					echo "<li>" . $name . " " . $size . "cl " . $price . "kr<br></li>";
+				}
+
+				?>
+				</ul>
 			</div>
 			<div class="col4 last">
 				
 			</div>
 		</div>
 	<?php require 'footer.php'; ?> 
-
-
 
 
