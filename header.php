191bdb39ef6c962156c733d0df71f185903b9b0a
Elias <omiros76@gmail.com>
2015-02-12 10:42:18 +0100

    conflict solved

--- header.php
+++ header.php
@@ -1,9 +1,18 @@
 <html>
 <head>
 	<title>E-handel</title>
+<<<<<<< Updated upstream
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<meta charset="UTF-8">
 
+=======
+
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+	<meta charset="UTF-8">
+
+
+>>>>>>> Stashed changes
 </head>
 <body>
 
