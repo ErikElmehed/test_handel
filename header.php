b7eafa03a433114a44a03a96d4db5b43d5597b83
inaschmitz <ina.e.schmitz@gmail.com>
2015-02-12 10:45:22 +0100

    fixed

--- header.php
+++ header.php
@@ -1,18 +1,8 @@
 <html>
 <head>
 	<title>E-handel</title>
-<<<<<<< Updated upstream
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<meta charset="UTF-8">
-
-=======
-
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-
-	<meta charset="UTF-8">
-
-
->>>>>>> Stashed changes
 </head>
 <body>
 
