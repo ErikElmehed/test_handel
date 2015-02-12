ec370baf67195ad2d7b8f6267b03f2d0dee03690
Cecilia Fredriksson <c.fredrikssons@gmail.com>
2015-02-12 11:18:04 +0100

    font awesome

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <head>
 	<title>E-handel</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 	<meta charset="UTF-8">
 </head>
 <body>
